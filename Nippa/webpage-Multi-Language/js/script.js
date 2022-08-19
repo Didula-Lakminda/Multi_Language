25215ca5d38db9d3d9bdfa5ff4ccfb6c041ceccd
Didula Lakminda <68744893+Didula-Lakminda@users.noreply.github.com>
2022-08-20 00:24:11 +0530

    Add files via upload

---
+++ Nippa/webpage-Multi-Language/js/script.js
@@ -0,0 +1,16 @@
+//English
+$(function() {
+    $(".en").click(function() {
+
+        $("h1").text("Hello");
+    });
+});
+
+
+//Sinhala
+$(function() {
+    $(".sl").click(function() {
+
+        $("h1").text("ආයුබෝවන්");
+    });
+});
